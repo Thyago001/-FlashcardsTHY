5733414ee3b5b1f3a576bd0ca31aa3eed6e0cdf6
Thyago001 <blackp27.t.a@gmail.com>
2024-11-07 22:00:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,75 @@
+// Programação
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação usada principalmente para desenvolvimento web.'
+);
+
+criaCartao(
+    'Programação',
+    'O que significa HTML?',
+    'HTML significa HyperText Markup Language, usada para criar a estrutura de páginas web.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória que armazena dados que podem ser alterados durante a execução do programa.'
+);
+
+// Matemática
+criaCartao(
+    'Matemática',
+    'O que é um número irracional?',
+    'Um número irracional é um número que não pode ser expresso como uma fração de dois inteiros.'
+);
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi?',
+    'Pi é uma constante matemática aproximadamente igual a 3,14159.'
+);
+
+criaCartao(
+    'Matemática',
+    'O que é uma matriz?',
+    'Em matemática, uma matriz é uma tabela de números organizada em linhas e colunas.'
+);
+
+// História
+criaCartao(
+    'História',
+    'Quem foi Alexandre, o Grande?',
+    'Alexandre, o Grande, foi um rei da Macedônia que criou um dos maiores impérios da história antiga.'
+);
+
+criaCartao(
+    'História',
+    'O que foi a Guerra Fria?',
+    'A Guerra Fria foi um período de tensão entre os Estados Unidos e a União Soviética após a Segunda Guerra Mundial.'
+);
+
+criaCartao(
+    'História',
+    'Quem foi Cleópatra?',
+    'Cleópatra foi a última rainha do Egito antigo, famosa por sua inteligência e alianças políticas.'
+);
+
+// Moda
+criaCartao(
+    'Moda',
+    'O que é fast fashion?',
+    'Fast fashion é um modelo de produção que busca criar moda rapidamente e a baixo custo.'
+);
+
+criaCartao(
+    'Moda',
+    'Quem foi Coco Chanel?',
+    'Coco Chanel foi uma estilista francesa que revolucionou a moda feminina no século XX.'
+);
+
+criaCartao(
+    'Moda',
+    'O que significa “vintage” na moda?',
+    'Vintage refere-se a peças de roupas que pertencem a uma época passada e são valorizadas por seu estilo clássico.'
+);
